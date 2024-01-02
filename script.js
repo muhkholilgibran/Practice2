721bb44bf6338ec0a2b36e01a5e37e4a21b39b7a
muhkholilgibran <oyigibran@gmail.com>
2024-01-02 16:57:37 +0800

    menambahkan file javascript

---
+++ script.js
@@ -0,0 +1,27 @@
+// Variables
+let variable1 = 'This is variable 1';
+let variable2 = 'This is variable 2';
+
+// Objects
+let person = {
+ firstName: 'John',
+ lastName: 'Doe',
+ age: 30,
+ haveMarried: true
+};
+
+// Arrays
+let numbers = [1, 2, 3, 4, 5];
+
+// Functions
+function addNumbers(num1, num2) {
+ return num1 + num2;
+}
+
+// Executing code and displaying results
+console.log(variable1);
+console.log(variable2);
+console.log(person);
+console.log(numbers);
+console.log(addNumbers(10, 20))
+console.log(person(haveMarried));
